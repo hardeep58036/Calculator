299806bae0aef6afb9d929a9fc20e94f0c0dc44d
Harry <hardeep58036@gmail.com>
2020-08-25 10:37:34 +0100

    Added documentation

--- CalculatorUI/wwwroot/AngularJS/service/Service.js
+++ CalculatorUI/wwwroot/AngularJS/service/Service.js
@@ -1,13 +1,12 @@
 ﻿//Service to get data from api
 myapp.service('calculatorService', function ($http) {
 
-    var baseUrl = "http://localhost:56873/"; //read from config. 
+    var baseUrl = "https://hardeepcalculatorapi.azurewebsites.net/"; //read from config. 
     var endPoint = "api/Calculate"; 
 
     this.calculate = function (number1, number2, operator) {
         var url = baseUrl + endPoint;
         var data = { "FirstNumber": parseFloat(number1), "SecondNumber": parseFloat(number2), "Operator": operator}, config = 'application/json';
-        //var url = 'https://calculatorapi20200824162058.azurewebsites.net/api/Calculate', data = { "FirstNumber": parseInt(number1), "SecondNumber": parseInt(number2), "Operator": operator}, config = 'application/json';
 
         return $http.post(url, data, config);
     }
